9b294337a6d6b5813826f9ac00218501907fb91f
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-02-24 03:14:43 +0100

    feat: add search results when no exact match

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,6 +12,16 @@ def index(request):
         for entry in util.list_entries():
             if entry.lower() == query.lower():
                 return HttpResponseRedirect(f'wiki/{entry}')
+        
+        # No exact match
+        results = []
+        for entry in util.list_entries():
+            if query.lower() in entry.lower():
+                results.append(entry)
+        return render(request, "encyclopedia/search_results.html", {
+            "results": results,
+            "query": query
+        })
     else:
         return render(request, "encyclopedia/index.html", {
             "entries": util.list_entries()
