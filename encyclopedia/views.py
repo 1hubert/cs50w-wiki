94f0e97be4491b5359c5d25383f14a59113b8f20
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-02-24 02:05:50 +0100

    feat: add entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,8 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry_title):
+    return render(request, "encyclopedia/entry.html", {
+        "entry_title": entry_title,
+        "entry_content": markdown2.markdown(util.get_entry(entry_title))
+    })
